2b2acde93430ea965f7c71a905e88c884982aa54
Chester <chesterdeveloper2000@gmail.com>
2020-08-26 13:49:15 +0800

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css"
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0"></div>
+        <div className="ma4 mt0">
+            <Tilt
+                className="Tilt br2 shadow-2"
+                options={{ max: 55 }}
+                style={{ height: 150, width: 150, cursor: "pointer" }}
+            >
+                <div className="Tilt-inner"> 🧠 </div>
+            </Tilt>
+        </div>
     );
 };
 
-export default Navigation;
+export default Logo;
